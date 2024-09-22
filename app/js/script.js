12004f8ff7b4da44e1e30e3807fc4a47fc3b3067
Mai Osama <mai.osama1001@gmail.com>
2024-09-22 15:24:41 +0300

    setting the toggle function
    setting the function of the toggle button to switch between the light and the dark mode

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,31 @@
-console.log("welcome to my new project")
+
+const selectColorMode = () => {
+    console.log(localStorage.getItem("colorMode"));
+    localStorage.getItem("colorMode") == "light" ? setLightMode() : setDarkMode()
+}
+
+
+
+function setDarkMode() {
+    document.querySelector("body").classList = "dark"
+}
+
+function setLightMode() {
+    document.querySelector("body").classList = "light"
+}
+
+selectColorMode();
+const radioButtons = document.querySelectorAll(".toggle__wrapper input");
+
+for (let i = 0; i < radioButtons.length; i++) {
+    console.log(radioButtons)
+    radioButtons[i].addEventListener('click', (event) => {
+        if (document.getElementById("dark").checked) {
+            localStorage.setItem("colorMode", "dark")
+        } else if (document.getElementById("light").checked) {
+            localStorage.setItem("colorMode", "light")
+
+        }
+    })
+}
+
