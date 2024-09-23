dbe82d1d89b656e1c82e346f19b850bd0da884b7
Mai Osama <mai.osama1001@gmail.com>
2024-09-23 15:07:47 +0300

    Setting the toggle and localStorage
    setting the toggle button to change the mode between dark and light, and set the web page to check the change happened in the system to change the mode accordingly

--- app/js/script.js
+++ app/js/script.js
@@ -1,29 +1,67 @@
 
-const selectColorMode = () => {
-    console.log(localStorage.getItem("colorMode"));
-    localStorage.getItem("colorMode") == "light" ? setLightMode() : setDarkMode()
-}
 
-
-
-function setDarkMode() {
-    document.querySelector("body").classList = "dark"
-}
-
-function setLightMode() {
-    document.querySelector("body").classList = "light"
-}
-
-selectColorMode();
+// setting the radio buttons
+const darkButton = document.getElementById("dark");
+const lightButton = document.getElementById("light");
 const radioButtons = document.querySelectorAll(".toggle__wrapper input");
 
+//to chenge the mode when the radio button is clicked
+const selectColorMode = () => {
+
+    if (localStorage.getItem("colorMode") == "light") {
+        setLightMode();
+        lightButton.click()
+    } else if (localStorage.getItem("colorMode") == "dark") {
+        setDarkMode();
+        darkButton.click()
+    }
+}
+
+
+//to change to the dark mode
+const setDarkMode = () => {
+    document.querySelector("body").classList = "dark";
+    localStorage.setItem('colorMode', 'dark')
+}
+
+// to change to the light mode
+const setLightMode = () => {
+    document.querySelector("body").classList = "light";
+    localStorage.setItem('colorMode', 'light')
+}
+
+//check when th eprefer color scheme change to change the color mode
+const checkModeChange = () => {
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (event) => {
+        console.log("checkModeChange");
+        checkMode()
+    })
+}
+
+//check the prefer color scheme for the oprating system and set the mode accordingly
+const checkMode = () => {
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        darkButton.click();
+        setDarkMode()
+    } else if ((window.matchMedia('(prefers-color-scheme: light)').matches)) {
+        lightButton.click();
+        setLightMode();
+    }
+}
+selectColorMode();
+checkMode();
+checkModeChange();
+
+//add the eventListener to the radio buttons and set the local staorage accordignly
 for (let i = 0; i < radioButtons.length; i++) {
     console.log(radioButtons)
     radioButtons[i].addEventListener('click', (event) => {
-        if (document.getElementById("dark").checked) {
-            localStorage.setItem("colorMode", "dark")
-        } else if (document.getElementById("light").checked) {
-            localStorage.setItem("colorMode", "light")
+        if (darkButton.checked) {
+            localStorage.setItem("colorMode", "dark");
+            setDarkMode();
+        } else if (lightButton.checked) {
+            localStorage.setItem("colorMode", "light");
+            setLightMode();
 
         }
     })
